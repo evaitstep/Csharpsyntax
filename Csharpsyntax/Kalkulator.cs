c30615918648c7efe77eb8e1d8b7e29798664e4d
CSharpFri <CSharpFri@CLASS2-05>
2017-11-24 21:22:46 +0100

    podmienky

---
+++ Csharpsyntax/Kalkulator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharpsyntax
+{
+    class Kalkulator
+    {
+        public static int secti(int a, int b)
+        {
+            int vysledek = a + b;
+            return vysledek;
+        }
+        public static int secti(int a, int b, int c)
+        {
+            int vysledek = a + b + c;
+            return vysledek;
+        }
+        public static int secti(string a, string b)
+        {
+            int vysledek = int.Parse(a) + int.Parse(b);
+          
+            return vysledek;
+        }
+    }
+}
--- Csharpsyntax/Program.cs
+++ Csharpsyntax/Program.cs
@@ -10,6 +10,44 @@ namespace Csharpsyntax
     {
         static void Main(string[] args)
         {
+            const int mojeKonstanta = 5;
+
+            int polomer = 6;
+            //polomer += 1;
+            Console.WriteLine("Puvodni polomer {0}", polomer);
+
+            int novypolomer = polomer++;
+            Console.WriteLine("Novy polomer {0}", novypolomer);
+            int zaseNovyPolomer = ++polomer;
+            Console.WriteLine("Novy polomer {0}", zaseNovyPolomer);
+
+            var vari = 6;
+
+            Kalkulator.secti("4", "5");
+            int vysledek =5;
+            if (vysledek == 4) vysledek++;
+            else
+                --vysledek;
+
+            switch (vysledek)
+            {
+                case 2:
+                    Console.WriteLine();
+                        break;
+
+            }
+
+
+
+            
+
+            if (((vysledek >= 10 && vysledek <= 20) || (vysledek >= 40 && vysledek <= 60)) && vysledek !=16 )
+            {
+                Console.WriteLine("Vysledek: {vysledek}");
+            }
+
+                Console.ReadLine();
+            
         }
     }
 }
